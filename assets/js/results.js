0b063865f27cbbfa6eeaf72357944a642ac4c630
Liv <148792974+L3xi4n@users.noreply.github.com>
2023-11-20 01:33:05 +0100

    Update results.js

--- assets/js/results.js
+++ assets/js/results.js
@@ -15,3 +15,61 @@ console.log(right)
 
 let wrong = queryStringArray[1]
 console.log(wrong)
+
+
+// creiamo la ciambella 
+window.onload = function () {
+  const chartData = {
+    labels: ['giuste', 'sbagliate'],
+    datasets: [
+      {
+        data: [right, wrong],
+        backgroundColor: ["#D20094", "#00FFFF"],
+        cutout: "65%",
+      },
+    ],
+  };
+
+const myChart = new Chart(document.getElementById("myChart"), {
+  type: "doughnut",
+  data: chartData,
+  options: {
+    borderWidth: 0,
+    hoverBorderWidth: 0,
+    borderRadius: 0,
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+}
+
+// esce il risultato in mezzo alla ciambella
+function doughnutText(){
+if(wrong > right){
+   document.getElementById("passedExam").classList.add('hidden');
+  }else{
+    document.getElementById("failedExam").classList.add('hidden');
+  }
+}
+
+doughnutText()
+
+// mettiamo le percentuali ai lati
+function percentage()
+{
+  const risposteEstatte = right * 10 + "%";
+  document.getElementById("percentageRight").innerText = risposteEstatte;
+  const risposteErrate = wrong * 10 + "%";
+  document.getElementById("percentageWrong").innerText = risposteErrate;
+}
+
+percentage();
+
+
+document.getElementById("counterQuestions1").innerText = right +"/10";
+
+document.getElementById("counterQuestions2").innerText = wrong +"/10";
