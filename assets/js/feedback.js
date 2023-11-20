db213d908facdae1810a8dfa29ce5914d1ed753e
Marco Furi <148861293+MarcoFuri@users.noreply.github.com>
2023-11-20 11:34:18 +0100

    Aggiornamento Feedback Build Week Giorno 5

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,9 +1,17 @@
 function coloraStelle(id) {
+  let star = document.getElementById("1");
+  let arrayStars = document.querySelectorAll("i");
+  if (star.style.color === "rgb(0, 255, 255)") {
+    arrayStars.forEach((element) =>
+      element.removeAttribute("style")); 
+  } else {
+    arrayStars.forEach((element) => element.removeAttribute("style"))
     for (i = 1; i < 11; i++) {
-      if (i <= id) {
-        document.getElementById(i).style.color = "#00ffffed";
-      } else {
-        document.getElementById(i).style.color = "#03082f";
-      }
+    if (i <= id) {
+      document.getElementById(i).style.color = "rgb(0, 255, 255)";
+    } else {
+      document.getElementById(i).style.color = "#03082f";
     }
   }
+  }
+}
