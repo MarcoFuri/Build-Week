daa27dd0767f37e8ac33cb05c353ca8d2ee9f8c0
Marco Furi <148861293+MarcoFuri@users.noreply.github.com>
2023-11-20 15:07:11 +0100

    Modifiche CSS

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,11 +1,6 @@
+// CLICK CAMBIO SELEZIONE
+
 function coloraStelle(id) {
-  let star = document.getElementById("1");
-  let arrayStars = document.querySelectorAll("i");
-  if (star.style.color === "rgb(0, 255, 255)") {
-    arrayStars.forEach((element) =>
-      element.removeAttribute("style")); 
-  } else {
-    arrayStars.forEach((element) => element.removeAttribute("style"))
     for (i = 1; i < 11; i++) {
     if (i <= id) {
       document.getElementById(i).style.color = "rgb(0, 255, 255)";
@@ -14,4 +9,26 @@ function coloraStelle(id) {
     }
    }
   }
-}
+
+
+
+
+// CLICK SCOMPARE, CLICK RISELEZIONA
+
+// function coloraStelle(id) {
+//   let star = document.getElementById("1");
+//   let arrayStars = document.querySelectorAll("i");
+//   if (star.style.color === "rgb(0, 255, 255)") {
+//     arrayStars.forEach((element) =>
+//       element.removeAttribute("style")); 
+//   } else {
+//     arrayStars.forEach((element) => element.removeAttribute("style"))
+//     for (i = 1; i < 11; i++) {
+//     if (i <= id) {
+//       document.getElementById(i).style.color = "rgb(0, 255, 255)";
+//     } else {
+//       document.getElementById(i).style.color = "#03082f";
+//     }
+//    }
+//   }
+// }
